c49b2322d6905dc2e027bc0cb9bcb61283cb0579
thrinath.d <thrinath.d@flipkart.com>
2016-05-07 16:02:25 +0530

    changes

--- app/src/main/java/com/example/thrinathd/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/thrinathd/courtcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thrinathd.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View teamA = findViewById(R.id.team_a);
     }
 
     public void threeIncrementerA(View view) {
